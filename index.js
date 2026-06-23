3d34710499e8a7a98e675c68d000a3fac5ce95db
0Lepe <163980004+0Lepe@users.noreply.github.com>
2026-06-22 22:29:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
 
 // Generar el codigo QR
 client.on('qr', function(qr) {
